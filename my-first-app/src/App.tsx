c8b8bf58769900f610cb1ca25d9c5cffcbd53d2d
HamidReza <hamid10631021@hotmail.com>
2024-12-27 10:09:57 +0330

    use state

--- my-first-app/src/App.tsx
+++ my-first-app/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  
+  // const [value, setValue] = useState(0);
+  
+  // const onclk = () => {
+  //   setValue(12);
+  //   console.log(value)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
