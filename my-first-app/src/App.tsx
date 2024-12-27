941fe6276763fae65c623429759c5831ad2f698f
HamidReza <hamid10631021@hotmail.com>
2024-12-27 10:25:35 +0330

    route in react

--- my-first-app/src/App.tsx
+++ my-first-app/src/App.tsx
@@ -1,20 +1,41 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useParams } from 'react-router-dom';
 
 const App: React.FC = () => {
-  
-  // const [value, setValue] = useState(0);
-  
-  // const onclk = () => {
-  //   setValue(12);
-  //   console.log(value)
-  // }
 
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div>
+            <header>header</header>
+            <main>
+                <Routes>
+                    <Route path='/' element={ <Home /> }/>
+                    <Route path='/shop' element={ <Shop /> }/>
+                    <Route path='/products/' element={ <Products /> }/>
+                    <Route path='/products/:id' element={ <Products /> }/>
+                </Routes>
+            </main>
+            <footer>footer</footer>
+        </div>
+    );
 }
+
+const Home = () => {
+    return <h2> Home</h2>
+}
+
+const Shop = () => {
+    return <h2> Shop</h2>
+}
+
+const Products = () => {
+    const {id} = useParams<"id">();
+    if (id) {
+        return <h2> Pruduct {id}</h2>
+    }
+    return <h2> Pruducts</h2>
+}
+
 
 export default App;
